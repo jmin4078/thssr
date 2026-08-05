7748e1bbfe138a9724dfb88c4cf5fdd4eaa54d9f
jmin4078 <wpalsrl8588@gmail.com>
2026-08-05 10:06:26 +0900

    Update book service and repository flow

--- src/main/java/org/example/thssr/controller/BookController.java
+++ src/main/java/org/example/thssr/controller/BookController.java
@@ -1,14 +1,19 @@
 package org.example.thssr.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.thssr.service.BookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+    private final BookService bookService;
+
     @GetMapping
-    public String page(){
+    public String page() {
         return "index";
     }
 }
