8956a781a9d5e2bdbba5ea35cfec6ddaef9820ef
jmin4078 <wpalsrl8588@gmail.com>
2026-08-05 10:44:39 +0900

    Update book listing flow

--- src/main/java/org/example/thssr/controller/BookController.java
+++ src/main/java/org/example/thssr/controller/BookController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -20,7 +21,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public String page() {
+    public String page(Model model) {
+        model.addAttribute("books", bookService.findAll());
         return "index";
     }
 
@@ -35,4 +37,11 @@ public class BookController {
         model.addAttribute("bookForm", BookFormDTO.builder().build());
         return "form";
     }
+    @PostMapping
+    public String createBook(
+            @ModelAttribute("bookForm") BookFormDTO bookFormDTO) {
+        bookService.createBook(bookFormDTO.toEntity());
+        return "redirect:/books";
+    }
+
 }
--- src/main/java/org/example/thssr/service/BookService.java
+++ src/main/java/org/example/thssr/service/BookService.java
@@ -2,9 +2,13 @@ package org.example.thssr.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.thssr.model.entity.BookEntity;
 import org.example.thssr.model.repository.BookRepository;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,4 +16,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class BookService {
     private final BookRepository bookRepository;
+
+    @Transactional
+    public void createBook(BookEntity entity) {
+        bookRepository.createBook(entity);
+    }
+
+    public List<BookEntity> findAll() {
+        return bookRepository.findAll();
+    }
 }
